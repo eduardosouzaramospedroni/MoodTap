7c998ea65a7d96f6e5bf2c2ce5e102796f854565
eduardosouzaramospedroni <edusouzapedroni@gmail.com>
2025-12-13 18:49:51 -0300

    Criada a aparência da segunda tela do app

--- app/src/main/java/com/example/moodtap/TelaInicio.java
+++ app/src/main/java/com/example/moodtap/TelaInicio.java
@@ -1,6 +1,8 @@
 package com.example.moodtap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TelaInicio extends AppCompatActivity {
+
+    Button botao_continuar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,13 @@ public class TelaInicio extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        botao_continuar = findViewById(R.id.button2);
+
+        botao_continuar.setOnClickListener(view -> {
+            Intent mudartela = new Intent(TelaInicio.this, TelaPrincipal.class);
+            startActivity(mudartela);
+        });
+
     }
 }
