30de67c1de294a8b7eb625f916850622d49f88c8
eduardosouzaramospedroni <edusouzapedroni@gmail.com>
2025-12-16 23:56:34 -0300

    Botão voltar da tela histórico, reajuste de cores e quinto build do projeto

--- app/src/main/java/com/example/moodtap/TelaHistorico.java
+++ app/src/main/java/com/example/moodtap/TelaHistorico.java
@@ -1,6 +1,8 @@
 package com.example.moodtap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TelaHistorico extends AppCompatActivity {
+
+    Button botao_voltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,13 @@ public class TelaHistorico extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        botao_voltar = findViewById(R.id.button3);
+
+        botao_voltar.setOnClickListener(view -> {
+            Intent mudartela = new Intent(TelaHistorico.this, TelaPrincipal.class);
+            startActivity(mudartela);
+        });
+
     }
 }
